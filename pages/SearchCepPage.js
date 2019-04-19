954e73bcd3546f72e5c81265cd8c29b35dab55ed
Marcelo Gonzalez <mfgonzalez@gmail.com>
2019-04-19 10:22:07 -0300

    Ajustes para build inicial

--- pages/SearchCepPage.js
+++ pages/SearchCepPage.js
@@ -1,20 +1,23 @@
 'use strict';
 
-class SearchCepPage {
-    constructor() {
-        var searchButton = element(by.css('input[type="submit"]'));
-        this.get = function () {
-            browser.waitForAngularEnabled(flase);
-            browser.get('http://www.buscacep.correios.com.br/sistemas/buscacep/buscaCepEndereco.cfm');
-        };
-        this.emptySearch = function () {
-            searchButton.click();
-        };
-        this.mandatoryAddressMessage = function () {
-            var ptor = protractor.getInstance();
-            var alertDialog = ptor.switchTo().alert();
-            return alertDialog.getText();
-        };
+var SearchCepPage = function() {
+
+    var searchButton = element(by.css('input[type="submit"]'));
+
+    this.get = function() {
+        browser.waitForAngularEnabled(flase);
+        browser.get('http://www.buscacep.correios.com.br/sistemas/buscacep/buscaCepEndereco.cfm');
     }
+    
+    this.emptySearch = function() {
+        searchButton.click();
+    }
+
+    this.mandatoryAddressMessage = function() {
+        var ptor = protractor.getInstance();
+        var alertDialog = ptor.switchTo().alert();
+        return alertDialog.getText();
+    }
+
 }
-export default SearchCepPage;
+module.exports = SearchCepPage;
