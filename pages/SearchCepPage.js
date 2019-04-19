689051631f9341373be2722355c6c082512c5d15
Marcelo Gonzalez <mfgonzalez@gmail.com>
2019-04-19 10:08:23 -0300

    Correcoes SearchCepPage.js

--- pages/SearchCepPage.js
+++ pages/SearchCepPage.js
@@ -1,17 +1,20 @@
 'use strict';
 
-var BuscaCepEnderecoPage = function() {
-
-    var searchButton = element(by.css('input[type="submit"]'));
-
-
-    this.get = function() {
-        browser.waitForAngularEnabled(flase);
-        browser.get('http://www.buscacep.correios.com.br/sistemas/buscacep/buscaCepEndereco.cfm');
+class SearchCepPage {
+    constructor() {
+        var searchButton = element(by.css('input[type="submit"]'));
+        this.get = function () {
+            browser.waitForAngularEnabled(flase);
+            browser.get('http://www.buscacep.correios.com.br/sistemas/buscacep/buscaCepEndereco.cfm');
+        };
+        this.emptySearch = function () {
+            searchButton.click();
+        };
+        this.mandatoryAddressMessage = function () {
+            var ptor = protractor.getInstance();
+            var alertDialog = ptor.switchTo().alert();
+            return alertDialog.getText();
+        };
     }
-    
-    this.emptySearch = function() {
-        searchButton.click();
-    }
-
 }
+export default SearchCepPage;
